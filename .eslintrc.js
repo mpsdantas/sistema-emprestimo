b8f73fa24bab333ace6ddef1f2e552c24597644f
Marcus Dantas <marcus.dantas@bct.ect.ufrn.br>
2018-04-09 18:24:36 -0300

    Criação das configurações iniciais do nodeJS

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-console":0,
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
