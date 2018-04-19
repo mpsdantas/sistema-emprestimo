77a3bd3a22f02783b573cb59f7c52e17b4b986b7
Marcus Dantas <mpsdantas15@gmail.com>
2018-04-19 08:19:17 -0300

    feat(/src/views): Rota de adicionar equipamento

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -3,4 +3,7 @@ module.exports = application => {
     application.get('/dashboard/index',routerAnalizer.protectRouterUser, (req,res) => {
         res.render('dashboard/index');
     });  
+    application.get('/dashboard/novo-equipamento', routerAnalizer.protectRouterUser, (req, res) => {
+        res.render('dashboard/novo-equipamento');
+    });
 };
