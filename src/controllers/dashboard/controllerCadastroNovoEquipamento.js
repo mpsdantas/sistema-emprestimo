eacb25d54f601d001f7ec03cfd8a72ca96500171
Marcus Dantas <marcus.dantas@bct.ect.ufrn.br>
2018-04-20 12:52:12 -0300

    feat(src/controllers/dashborad): AJuste controllers views

--- src/controllers/dashboard/controllerCadastroNovoEquipamento.js
+++ src/controllers/dashboard/controllerCadastroNovoEquipamento.js
@@ -1,4 +1,20 @@
-exports.makeCadastro = (application, req, res) =>{
-    console.log(req.body);
-    res.status(200).json({status:true});
+const mongoose = require('mongoose');
+const Equipamento = mongoose.model('Equipamentos');
+
+exports.makeCadastro = async (application, req, res) => {
+    req.assert('nome', 'O nome do equipamento não pode ser vazio.').notEmpty();
+    req.assert('qntdTotal', 'A quantidade não pode ser vazia.').notEmpty();
+    req.assert('categoria', 'A categoria não pode ser vazia.').notEmpty();
+    
+    const erros = req.validationErrors();
+    if(erros){
+        res.status(200).json({erroForm:true,erros:erros});
+        return;
+    }
+
+    req.body.qntdDisponivel = req.body.qntdTotal;
+    const novoEquipamento = new Equipamento(req.body);
+    await novoEquipamento.save();
+
+    res.status(200).json({status:true, msg: "Equipamento cadastrado com sucesso."});
 };
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,11 +1,13 @@
 const routerAnalizer = require('../controllers/protectRouter');
 const controllerNovoCadastro = require('../controllers/dashboard/controllerCadastroNovoEquipamento');
+const controllerViewEquipamento = require('../controllers/dashboard/controllerViewCadastroEquipamento');
+
 module.exports = application => {
     application.get('/dashboard/index',routerAnalizer.protectRouterUser, (req,res) => {
         res.render('dashboard/index');
     });  
     application.get('/dashboard/novo-equipamento', routerAnalizer.protectRouterUser, (req, res) => {
-        res.render('dashboard/novo-equipamento');
+        controllerViewEquipamento.renderEquipamentos(application, req, res);
     });
     application.post('/dashboard/novo-equipamento', routerAnalizer.protectRouterUser, (req, res) => {
         controllerNovoCadastro.makeCadastro(application, req, res);
