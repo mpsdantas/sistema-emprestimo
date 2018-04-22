87ee492e8394116e6a1d22a7713876654880f13a
Marcus Dantas <mpsdantas15@gmail.com>
2018-04-22 00:49:06 -0300

    feat(src/controllers/dashboard/): Criação do controlador que realiza o emprestimo de equipamentos.

---
+++ src/controllers/dashboard/controllerRealizarEmprestimo.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Emprestimo= mongoose.model('Emprestimos');
+const Equipamento = mongoose.model('Equipamentos');
+exports.realizarEmprestimo = async (application, req, res) =>{
+    if(req.body.email=="" || req.body.equipamento=="" || req.body.qntdEquipamento==""){
+        res.status(200).json({erro:true, msg:"Existem erros no formulário verifique se você preencheu todos os campos."});
+        return;
+    }
+    const buscaEquipamento = await Equipamento.findOne({nome:req.body.equipamento});
+    if (buscaEquipamento.qntdDisponivel <= parseInt(req.body.qntdEquipamento)){
+        res.status(200).json({ erro: true, msg: "A quantidade informada é maior do que a quantidade disponível." });
+        return;
+    }
+    const novoEmprestimo = new Emprestimo({
+        status: true,
+        emailDono: req.session.email,
+        emailUsuarioEmprestado: req.body.email,
+        itemEmprestado: {
+            nomeEquip: buscaEquipamento.nome,
+            qntdEmprestada: req.body.qntdEquipamento
+        },
+        dataEmprestimo: new Date() 
+    });
+    await novoEmprestimo.save();
+    await Equipamento.updateOne({ nome: buscaEquipamento.nome }, { $set: { qntdDisponivel: (buscaEquipamento.qntdDisponivel - req.body.qntdEquipamento)}});
+    res.status(200).json({ status: true, msg: "Emprestimo realizado com sucesso." });
+    return;
+}
