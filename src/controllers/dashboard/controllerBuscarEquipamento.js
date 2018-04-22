a2f70f5a55f622151ec3ae6dc02f9631b0e25a8b
Marcus Dantas <mpsdantas15@gmail.com>
2018-04-22 00:48:17 -0300

    feat(src/controllers/dashboard/): Criação do controlador que faz busca de equipamentos.

---
+++ src/controllers/dashboard/controllerBuscarEquipamento.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Equipamento = mongoose.model('Equipamentos');
+const matchSorter = require('match-sorter');
+exports.buscarEquipamento = async (application, req, res) =>{
+    if(req.body.valorPesquisa==""){
+        res.status(200).json({erro:true, msg:"Insira algum valor para realizar a pesquisa"});
+        return;
+    }
+    const todosEquipamentos = await Equipamento.find({});
+    const equipamentoFiltrado = matchSorter(todosEquipamentos, req.body.valorPesquisa, { keys: ['nome', 'codigo'] });
+    res.status(200).json(equipamentoFiltrado);
+}
