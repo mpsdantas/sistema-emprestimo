448b06b059fcbaedb4626efb1c8d8da881858a17
Marcus Dantas <marcus.dantas@bct.ect.ufrn.br>
2018-04-24 16:31:52 -0300

    fix(src/controllers/): Ajuste da vizualização dos equipamentos.

--- src/controllers/dashboard/controllerViewTodosEquipamentos.js
+++ src/controllers/dashboard/controllerViewTodosEquipamentos.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 const Equipamento = mongoose.model('Equipamentos');
 
 exports.renderTodosEquipamentos = async (application, req, res) => {
-    const todosEquipamentos = await Equipamento.find({});
+    const todosEquipamentos = await Equipamento.find({idDono: req.session._id});
     res.render('dashboard/todos-equipamentos', {todosEquipamentos});
 };
